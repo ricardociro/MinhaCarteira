b083ee401722d420bbaba2faf1d6305a0df8f6a1
ricardociro <ciro.ignacio@gmail.com>
2021-02-20 14:28:30 -0300

    troca de porta

--- temporario.js
+++ temporario.js
@@ -10,7 +10,5 @@ app.use(routes);
 
 //const server = http.Server(app);
 
-app.listen(process.env.PORT || 3000);
-console.log('porta');
-console.log(process.env.PORT);
-console.log('iniciado com sucesso');
+app.listen(process.env.PORT || 21077);
+
