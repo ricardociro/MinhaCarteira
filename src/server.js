36df1f6bc22bc2b767f867f164c08797cf9f610e
ricardociro <ciro.ignacio@gmail.com>
2021-02-20 12:21:22 -0300

    aleracao server.js

--- src/server.js
+++ src/server.js
@@ -11,9 +11,9 @@ app.use(express.json());
 app.use(routes);
 
 //server.listen(3000);
-const PORT = process.env.PORT || process.env.WEBCHAT_SERVER || 3000;
-server.listen(3000, function () {
-  console.log('Servidor rodando em: 3000');
+const PORT = process.env.PORT || process.env.SERVER || 3000;
+server.listen(21077, function () {
+  console.log('Servidor rodando em: 21077');
 });
 
 //npm install express
