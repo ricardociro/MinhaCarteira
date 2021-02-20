c0a4ed0b020beec0b74b2cdd5bd38e83ccd49fb5
ricardociro <ciro.ignacio@gmail.com>
2021-02-20 12:28:55 -0300

    alterado porta  condicao

--- src/server.js
+++ src/server.js
@@ -11,16 +11,7 @@ app.use(express.json());
 app.use(routes);
 
 //server.listen(3000);
-const PORT = process.env.PORT || process.env.SERVER || 3000;
+const PORT = process.env.PORT || process.env.BACK_SERVER || 3000;
 server.listen(21077, function () {
   console.log('Servidor rodando em: 21077');
 });
-
-//npm install express
-//npm install nodemon -D
-//npm install knex
-//npm install mysql
-//npx knex init
-//npm install cors
-//rodar aplicacao
-// node index.js
