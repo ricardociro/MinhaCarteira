f592e89fd3f14f4575ccb2dfeb09c7d5fd2d7884
ricardociro <ciro.ignacio@gmail.com>
2021-02-20 14:01:00 -0300

    ajuste console log

--- temporario.js
+++ temporario.js
@@ -11,5 +11,6 @@ app.use(routes);
 //const server = http.Server(app);
 
 app.listen(process.env.PORT || 3000);
-
+console.log('porta');
+console.log(process.env.PORT);
 console.log('iniciado com sucesso');
