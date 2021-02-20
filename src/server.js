3197b30763d88be202057aaf5841c4ea144c948f
ricardociro <ciro.ignacio@gmail.com>
2021-02-20 12:35:00 -0300

    alterado porta 21077

--- src/server.js
+++ src/server.js
@@ -10,8 +10,5 @@ const server = http.Server(app);
 app.use(express.json());
 app.use(routes);
 
-//server.listen(3000);
-const PORT = process.env.PORT || process.env.BACK_SERVER || 3000;
-server.listen(21077, function () {
-  console.log('Servidor rodando em: 21077');
-});
+server.listen(21077);
+
